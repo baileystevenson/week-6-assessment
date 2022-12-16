149c3e60907bab8507c784126fdaaae25f94a0c9
Amber Stevenson <amber.stevenson14@gmail.com>
2022-12-16 10:53:41 -0700

    first commit

---
+++ functions.test.ts
@@ -0,0 +1,10 @@
+const {shuffleArray} = require('./utils')
+
+
+
+
+describe('shuffleArray should', () => {
+    test('should be equal to "shuffleArry" () => {
+        expect(shuffleArray should).toBe('array')
+    })
+})
